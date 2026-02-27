5be3f7b05b7f3f97468a74133af7dd0d6b89886f
ESAKKIMADAN <esakkimadan10@gmail.com>
2026-02-27 09:17:35 +0530

    UC6 - Queue + Stack Based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,24 +1,38 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
-
     public static void main(String[] args) {
-        String input = "radar";
-        char[] chars = input.toCharArray();
-        int start = 0;
-        int end = chars.length - 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string to check if it's a palindrome: ");
+        String originalString = scanner.nextLine();
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < originalString.length(); i++) {
+            char c = originalString.charAt(i);
+            queue.add(c);
+            stack.push(c);
+        }
+
         boolean isPalindrome = true;
-        while (start < end) {
-            if (chars[start] != chars[end]) {
+
+        while (!queue.isEmpty() && !stack.isEmpty()) {
+            char queueChar = queue.remove();
+            char stackChar = stack.pop();
+
+            if (queueChar != stackChar) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
-        if (isPalindrome)
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not Palindrome");
+        if (isPalindrome) {
+            System.out.println("Result: The string is a Palindrome.");
+        } else {
+            System.out.println("Result: The string is not a Palindrome.");
+        }
+
+        scanner.close();
     }
 }
